d8e51683a1d92bd0e0569ab47f6582947aa28b89
Ana Carolina <novais.acarol@gmail.com>
2026-03-09 14:12:58 -0300

    Clean up comments in calculadoraraizquadrada.py
    Removed outdated comment and updated date format.

--- calculadoraraizquadrada.py
+++ calculadoraraizquadrada.py
@@ -1,5 +1,5 @@
  #calculadora de raiz quadrada
- #não fiz quuando mas fiz essa semana 09/02/26
+ #09/02/26
 while True:
     numero = int(input("Digite um número para ver sua raiz quadrada (ou 0 para sair):"))
     if numero == 0:
